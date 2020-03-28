4a19e24cc68474f80ecb41b20c43f81a2c5afd78
stoleS <predragstosic29@gmail.com>
2020-03-28 01:07:22 +0100

    Initial games service

--- api/src/controllers/games.js
+++ api/src/controllers/games.js
@@ -1,10 +1,16 @@
-const fetch = require('node-fetch')
-const $ = require('cheerio')
 const { createResponse } = require('../utils/response-builder')
+
+/* -------------------------------------------------------------------------- */
+/*                              Service functions                             */
+/* -------------------------------------------------------------------------- */
+
+const { getGameData } = require('../services/games/scrape')
+
+/* --------------------- Scrape game data from provider --------------------- */
 
 const scrapeGameData = async (req, res, next) => {
   const { url } = req.body
-  const gameData = { url }
+  const gameData = getGameData(url)
   createResponse(null, res, 200, gameData)
 }
 
---
+++ api/src/services/games/scrape.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+const $ = require('cheerio')
+
+const getGameData = async url => {
+  try {
+    return {}
+  } catch (error) {}
+}
+
+module.exports = { getGameData }
